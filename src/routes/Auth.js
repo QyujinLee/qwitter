852dc59354a8adf9de26898b9e8d6552cc91c784
QyujinLee <gyujin89@gmail.com>
2021-02-19 18:04:31 +0900

    ~ #2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,76 @@
-import React from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+    const onChange = (e) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await authService.createUserWithEmailAndPassword(
+                    email,
+                    password
+                );
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(
+                    email,
+                    password
+                );
+            }
+
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={onChange}
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={onChange}
+                    required
+                />
+                <input
+                    type="submit"
+                    value={newAccount ? "Create Account" : "Log In"}
+                />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
