de588027330998c2b46cb31815e557c43436f425
QyujinLee <gyujin89@gmail.com>
2021-02-23 17:48:57 +0900

    #3 Nweeting

---
+++ src/components/Qweet.js
@@ -0,0 +1,69 @@
+import { dbService } from "fbase";
+import React, { useState } from "react";
+
+const Qweet = ({ qweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newQweet, setNewQweet] = useState(qweetObj.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm(
+            "Are you sure you want to delete this qweet ? "
+        );
+
+        if (ok) {
+            // delete qweet
+            await dbService.doc(`qweets/${qweetObj.id}`).delete();
+        }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.doc(`qweets/${qweetObj.id}`).update({
+            text: newQweet,
+        });
+
+        setEditing(false);
+    };
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+
+        setNewQweet(value);
+    };
+
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Edit your Qweet"
+                            value={newQweet}
+                            onChange={onChange}
+                            required
+                        />
+                        <input type="submit" value="Update Qweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <h4>{qweetObj.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>
+                                Delete Qweet
+                            </button>
+                            <button onClick={toggleEditing}>Edit Qweet</button>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Qweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,78 @@
-import React from "react";
+import Qweet from "components/Qweet";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+    const [qweet, setQweet] = useState("");
+    const [qweets, setQweets] = useState([]);
+
+    // 아래와 같은 방법은 오래된 데이터를 가져온다
+    // const getQweets = async () => {
+    //     const dbQweets = await dbService.collection("qweets").get();
+    //     dbQweets.forEach((document) => {
+    //         const qweetObj = {
+    //             ...document.data(),
+    //             id: document.id,
+    //         };
+    //         setQweets((prev) => [qweetObj, ...prev]);
+    //     });
+    // };
+
+    useEffect(() => {
+        dbService
+            .collection("qweets")
+            .orderBy("createdAt", "desc")
+            .onSnapshot((snapshot) => {
+                const qweetArray = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setQweets(qweetArray);
+            });
+    }, []);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.collection("qweets").add({
+            text: qweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+        });
+
+        setQweet("");
+    };
+
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+
+        setQweet(value);
+    };
+
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="What's on your mind ?"
+                    maxLength={120}
+                    value={qweet}
+                    onChange={onChange}
+                />
+                <input type="submit" value="Qweet" onClick={onSubmit} />
+            </form>
+            <div>
+                {qweets.map((qweet) => (
+                    <Qweet
+                        key={qweet.id}
+                        qweetObj={qweet}
+                        isOwner={userObj.uid === qweet.creatorId}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Home;
